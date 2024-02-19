92f129e5c7f373fcb5d975166db9f5baa86691b5
Ryan Rodriguez <116002606+RyanRuno@users.noreply.github.com>
2024-02-18 16:39:24 -0800

    Add files via upload
    Second Project on Visual Studio Code at Mesa College

---
+++ TaxSolution/TaxProject/MainForm.vb
@@ -0,0 +1,34 @@
+﻿'Soluton Name: Tax Solution
+'Project Name: Tax Calculator Project
+'Programmer Name: Ryan Rodriguez
+'Date Created: 9/11/23
+'Project Description: Calculate and display the Tax from user amount.
+'
+'
+Option Explicit On
+Option Strict On
+Option Infer Off
+Public Class frmMain
+    Const dblRate As Double = 0.0135
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'Calculate the tax
+
+        Dim dblCurrent As Double
+        Dim dblincrease As Double
+
+
+        Double.TryParse(txtAssessed.Text, dblCurrent)
+        dblincrease = dblCurrent * dblRate
+
+
+        lbltxt.Text = dblincrease.ToString("C2")
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close Program
+
+        Me.Close()
+    End Sub
+End Class
