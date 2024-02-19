48753873e9413fcdd485925592a858420216206f
Ryan Rodriguez <116002606+RyanRuno@users.noreply.github.com>
2024-02-18 16:49:38 -0800

    First Project on Visual Studio Code

---
+++ FloristHavenSolution/DisplayHoursProject/MainForm.vb
@@ -0,0 +1,25 @@
+﻿'Soluton Name:        Tax Solution
+'Project Name:        Tax Calculator Project
+'Programmer Name:     Ryan Rodriguez
+'Date Created:        8/30/23
+'Project Description: Display the hours or hide the hours available
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+Public Class frmMain
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        'Show the hours
+        pbHours.Visible = True
+    End Sub
+
+    Private Sub btnHide_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+        'Hide the hours
+        pbHours.Visible = False
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close application
+        Me.Close()
+    End Sub
+End Class
